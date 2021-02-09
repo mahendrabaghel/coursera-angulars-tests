9b2b1e9bd18f5b072d8df49c48ba36f88be0e7e2
Mahendra S Baghel <msbaghel@hotmail.com>
2021-02-09 16:38:40 -0500

    Updated code for the assignment

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -5,19 +5,29 @@ angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject=["$scope"];
+LunchCheckController.$inject=['$scope'];
 
-function MessageAppController($scope) {
+function LunchCheckController($scope) {
   $scope.items="1,2,3";
   $scope.diet = "";
 
   $scope.checkDiet = function () {
-    var lItems = $scope.items;
-    var aItem = lItems.split(',');
-    if(aItem.length<=3){
+    var locItems = $scope.items.trim();
+    var itemArr = locItems.split(',');
+    if(itemArr.length<=3){
       $scope.diet = "Enjoy!";
     }else{
-      $scope.diet = "Too much!"
+      $scope.diet = "Too much!";
+    }
+  };
+
+  $scope.clearDiet = function () {
+    var locItems = $scope.items.trim();
+    console.log(locItems);
+    var itemArr = locItems.split(',');
+    console.log(itemArr);
+    if(locItems.length<1){
+      $scope.diet = "";
     }
   };
 
