3e7886ccdaa098b3a83ad69154fd80db54154ae2
Mahendra S Baghel <msbaghel@hotmail.com>
2021-02-09 17:37:34 -0500

    Fixed the code, including one extra point.

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,37 +1,60 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('LunchCheck', [])
+  angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', LunchCheckController);
+    .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject=['$scope'];
+  LunchCheckController.$inject = ['$scope'];
 
-function LunchCheckController($scope) {
-  $scope.items="1,2,3";
-  $scope.diet = "";
+  function LunchCheckController($scope) {
+    $scope.items = "";
+    $scope.diet = "";
+    $scope.fColor="";
 
-  $scope.checkDiet = function () {
-    var locItems = $scope.items.trim();
-    var itemArr = locItems.split(',');
-    if(itemArr.length<=3){
-      $scope.diet = "Enjoy!";
-    }else{
-      $scope.diet = "Too much!";
-    }
-  };
+    $scope.checkDiet = function () {
+      
+      var locItems = $scope.items.trim();
 
-  $scope.clearDiet = function () {
-    var locItems = $scope.items.trim();
-    console.log(locItems);
-    var itemArr = locItems.split(',');
-    console.log(itemArr);
-    if(locItems.length<1){
-      $scope.diet = "";
-    }
-  };
+      //check if there are items.
+      if(locItems.length<1){
+        $scope.diet = "Please enter data first";
+        $scope.fColor="red";
+        return;
+      }
+
+      var itemArr = locItems.split(',');
+      var citemArr = [];
+
+      //remove empty items from the textbox
+      for (var i = 0; i < itemArr.length; i++) {
+        if (itemArr[i].trim().length > 1) {
+          citemArr[i] = itemArr[i];
+        }
+      }
+
+      //finally count the legit items
+      if (citemArr.length <= 3) {
+        $scope.diet = "Enjoy!";
+        $scope.fColor="green";
+      } else {
+        $scope.diet = "Too much!";
+        $scope.fColor="green";
+      }
+    };
+
+    $scope.clearDiet = function () {
+      var locItems = $scope.items.trim();
+      //console.log(locItems);
+      var itemArr = locItems.split(',');
+      //console.log(itemArr);
+      if (locItems.length < 1) {
+        $scope.diet = "Please enter data first";
+        $scope.fColor="red";
+      }
+    };
 
 
-}
+  }
 
 })();
