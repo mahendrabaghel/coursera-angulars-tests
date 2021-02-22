2a64942c0f4b1f46c045ca2462bc28be1048a6e6
Mahendra Baghel <msbaghel@hotmail.com>
2021-02-21 23:39:01 -0500

    Second Service

--- SandBox/CustomServices/firstService.js
+++ SandBox/CustomServices/firstService.js
@@ -1,48 +1,79 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListAddController', ShoppingListAddController)
-.controller('ShoppingListShowController', ShoppingListShowController)
-.service('ShoppingListService', ShoppingListService);
+angular.module('ControllerAsApp', [])
+.controller('ShoppingListController1', ShoppingListController1)
+.controller('ShoppingListController2', ShoppingListController2)
+.factory('ShoppingListFactory', ShoppingListFactory);
 
-ShoppingListAddController.$inject = ['ShoppingListService'];
-function ShoppingListAddController(ShoppingListService) {
-  var itemAdder = this;
+// LIST #1 - controller
+ShoppingListController1.$inject = ['ShoppingListFactory'];
+function ShoppingListController1(ShoppingListFactory) {
+  var list1 = this;
 
-  itemAdder.itemName = "";
-  itemAdder.itemQuantity = "";
+  // Use factory to create new shopping list service
+  var shoppingList = ShoppingListFactory();
 
-  itemAdder.addItem = function () {
-    ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+  list1.items = shoppingList.getItems();
+
+  list1.itemName = "";
+  list1.itemQuantity = "";
+
+  list1.addItem = function () {
+    shoppingList.addItem(list1.itemName, list1.itemQuantity);
   }
-}
 
-
-ShoppingListShowController.$inject = ['ShoppingListService'];
-function ShoppingListShowController(ShoppingListService) {
-  var showList = this;
-
-  showList.items = ShoppingListService.getItems();
-
-  showList.removeItem = function (itemIndex) {
-    ShoppingListService.removeItem(itemIndex);
+  list1.removeItem = function (itemIndex) {
+    shoppingList.removeItem(itemIndex);
   };
 }
 
 
-function ShoppingListService() {
+// LIST #2 - controller
+ShoppingListController2.$inject = ['ShoppingListFactory'];
+function ShoppingListController2(ShoppingListFactory) {
+  var list2 = this;
+
+  // Use factory to create new shopping list service
+  var shoppingList = ShoppingListFactory(3);
+
+  list2.items = shoppingList.getItems();
+
+  list2.itemName = "";
+  list2.itemQuantity = "";
+
+  list2.addItem = function () {
+    try {
+      shoppingList.addItem(list2.itemName, list2.itemQuantity);
+    } catch (error) {
+      list2.errorMessage = error.message;
+    }
+
+  }
+
+  list2.removeItem = function (itemIndex) {
+    shoppingList.removeItem(itemIndex);
+  };
+}
+
+//service declaration
+function ShoppingListService(maxItem) {
   var service = this;
 
   // List of shopping items
   var items = [];
 
   service.addItem = function (itemName, quantity) {
-    var item = {
-      name: itemName,
-      quantity: quantity
-    };
-    items.push(item);
+    if((maxItem === undefined) || (maxItem !== undefined) &&
+    (items.length < maxItem)){
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      items.push(item);
+    }else {
+      throw new Error("Max items "+maxItem+" reached");
+    }
   };
 
   service.removeItem = function (itemIndex) {
@@ -52,6 +83,14 @@ function ShoppingListService() {
   service.getItems = function () {
     return items;
   };
+}//service ends
+
+function ShoppingListFactory() {
+  var factory = function (maxItems) {
+    return new ShoppingListService(maxItems);
+  };
+
+  return factory;
 }
 
 })();
